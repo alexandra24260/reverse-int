8e5f14244782046c50680c78b28c06b8f55139b1
alexandra.Vecher <alexandra_vecher@epam.com>
2021-03-11 11:32:43 +0300

    solution for reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+let arr = Array.from(String(n), Number);
+let i;
+let changedArray = [];
+for ( i = 0; i <= arr.length; i ++){
+    if (!isNaN(arr[i])){
+        changedArray += arr[i];
+    }
+}
+return changedArray.split('').reverse().join('');
 }
